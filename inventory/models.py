c7e80196156ee3485ec73570004c367a5045f060
nathanielpatrick.lemana <nathanielpatrick.lemana@snhu.edu>
2021-03-03 02:42:02 -0500

    fixed ITEM

--- inventory/models.py
+++ inventory/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    restock_quantity = models.IntegerField()
+    restock_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
 class MenuItems(models.Model):
     name = models.CharField(max_length=50)
     quantity = models.IntegerField()
