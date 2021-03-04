9033b20082f28e6e45be03fa3fac86970519cf42
anthony-aleman <58735466+anthony-aleman@users.noreply.github.com>
2021-03-03 19:06:34 -0500

    Error in database?

--- inventory/models.py
+++ inventory/models.py
@@ -4,11 +4,12 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=50)
     quantity = models.IntegerField()
-    restock_quantity = models.IntegerField()
-    restock_date = models.DateField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
 
 class MenuItems(models.Model):
     name = models.CharField(max_length=50)
